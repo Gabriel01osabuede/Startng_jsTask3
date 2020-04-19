34128fb523ece8272299ce7cb37699d571ce0fbe
Gabriel01osabuede <51996566+Gabriel01osabuede@users.noreply.github.com>
2020-04-19 09:07:00 +0100

    made and html file

--- task3_yugiho.js
+++ task3_yugiho.js
@@ -1,11 +1,14 @@
 'use strict'
 
+// creating and array to store the data being given
 let data_array = [];
-
+// creating a function to do the calculation
 function datainput(data){
-
+// using a for loop to iterate through the input given
   for (var i = 1; i <= data; i++){
+    // using conditions to manipulate the data passed in.
     if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
+      // pushing the manipulated data to the array
       data_array.push('yu-gi-oh');
     }else if (i % 2 == 0 && i % 3 == 0){
       data_array.push('yu-gi');
@@ -26,7 +29,9 @@ function datainput(data){
 
 }
 
-
+// accepting input from the user
 var input = +prompt("Put in a number: ");
+// calling the function
 datainput(input);
+// logging data_array created already
 console.log(data_array);
